99fe5a5e71103fff7bcda47899ffe4f55641e37c
Junn <socker210@gmail.com>
2023-04-18 05:37:38 +0900

    update api url & dist path

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,71 @@
 // Delcare Webpack Plugins
-var htmlWebpackPlugin = require('html-webpack-plugin');
-
+var htmlWebpackPlugin = require('html-webpack-plugin')
 
 // Export Webpack Settings
 module.exports = {
-	entry: __dirname + "/www/index.js" ,
+	entry: __dirname + '/www/index.js',
 	output: {
-		path: __dirname + "/www/dist" ,
-		filename: "bundle.js" ,
-		publicPath: "/"
+		path: __dirname + '/public',
+		filename: 'bundle.js',
+		publicPath: '/',
 	},
 	resolve: {
-		alias: { 
-			api: __dirname + "/www/api.js" ,
-			meta: __dirname + "/www/meta.js"
-		}
+		alias: {
+			api: __dirname + '/www/api.js',
+			meta: __dirname + '/www/meta.js',
+		},
 	},
 	devServer: {
 		progress: true,
-		historyApiFallback: true
+		historyApiFallback: true,
 	},
 	module: {
-		rules: [{
-			test: /\.js?/ ,
-			exclude: /node_modules/ ,
-			use: [{
-				loader: "babel-loader" ,
-				query: {
-					presets: ["es2015","react"]
-				}
-			}]
-		},
-		{
-			test: /\.css?/ ,
-			use: ["style-loader",{
-				loader: "css-loader" ,
-				query: {
-					modules: true ,
-					localIdentName: "[local]"
-				}
-			}]
-		},
-		{
-			test: /\.(svg|eof|ttf|woff|woff2)$/ ,
-			use: [{
-				loader: "url-loader" ,
-				query: {
-					limit: 5000 ,
-					name: "assets/fonts/[name]__[hash].[ext]"
-				}
-			}]
-		}]
+		rules: [
+			{
+				test: /\.js?/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						query: {
+							presets: ['es2015', 'react'],
+						},
+					},
+				],
+			},
+			{
+				test: /\.css?/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						query: {
+							modules: true,
+							localIdentName: '[local]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.(svg|eof|ttf|woff|woff2)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						query: {
+							limit: 5000,
+							name: 'assets/fonts/[name]__[hash].[ext]',
+						},
+					},
+				],
+			},
+		],
 	},
 	plugins: [
 		new htmlWebpackPlugin({
-			title: "Keep memorize your own" ,
-			hash: true ,
-			template: "www/template.html"
-		})
-	]
+			title: 'Keep memorize your own',
+			hash: true,
+			template: 'www/template.html',
+			favicon: 'www/favicon.ico',
+		}),
+	],
 }
